6a2d0bb4628044a0552e042ddd7a84d8ef224f24
Shineylo <Bryan.artiges@gmail.com>
2023-03-29 12:15:06 +0200

    Initial commit

---
+++ src/main/java/technobel/bart/makerhub/MakerHubApplication.java
@@ -0,0 +1,13 @@
+package technobel.bart.makerhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakerHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakerHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/technobel/bart/makerhub/MakerHubApplicationTests.java
@@ -0,0 +1,13 @@
+package technobel.bart.makerhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakerHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
